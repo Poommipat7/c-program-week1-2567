710a2d6baf11678c1647b7932b45905f091c16c2
Poommipat7 <poommipat462@gmail.com>
2024-05-24 16:06:43 +0700

    poommipat

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    //Create integer variable
+    int age = 15;
+    //print Hello World to the screen
+    printf("Hello World! \n");
+    printf("My name is Mr.Poommipat \n");
+    printf("Age: %d\n",age);
+    return 0;
+}
